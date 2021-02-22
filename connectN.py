a671fa69e2e3ab9ce2c9139b481ed556ca2c2716
tvandenboom <77467241+tvandenboom@users.noreply.github.com>
2021-02-22 16:09:47 -0500

    Update connectN.py

--- connectN.py
+++ connectN.py
@@ -1,7 +1,5 @@
 import numpy as np
 
-#Create a class defining an instance of a Connect Four board.
-
 class ConnectFour:
     win_length = 4
     num_rows = 6
@@ -13,8 +11,10 @@ class ConnectFour:
     def __init__(self):
         #On initialization, the board_state is empty and the turn is 1
         self.board_state = np.zeros((self.num_rows,self.num_cols), dtype=int)
-        self.turn = 1
+        self.turn = 0
         self.is_won = False
+        self.legal_moves = np.arange(7)
+        self.last_move = (-1,-1)
 
     def play_chip(self, column):
         #First check if the game is over:
@@ -23,76 +23,89 @@ class ConnectFour:
         else:
             #Player whose turn it is places a chip in column
             #Determine whose turn it is:
-            if self.turn % 2 == 1:
+            if self.turn % 2 == 0:
                 chip = 1
             else:
                 chip = -1
-            #Determine the legal moves in the board_state:
-            self.legal_moves = np.where(self.board_state[0]==0)[0]
 
             #If suggested move is legal, play the chip in column and go to next turn.
             if column in self.legal_moves:
-                self.board_state[np.amax(np.where(self.board_state[:,column] == 0)),column] = chip
+                self.last_move = np.amax(np.where(self.board_state[:,column] == 0)),column
+                self.board_state[self.last_move] = chip
                 self.turn += 1
             else:
                 print('Column is full; please suggest another move.')
 
-    def check_win(self):
-        #Checks for a winning position for both players.
-        #Check for row wins
-        for i in range(self.num_rows-1,-1,-1):
-            #Check if row is empty
-            if any(self.board_state[i,:] != 0):
-                #Check if row has enough chips for a win
-                if (len(np.where(self.board_state[i,:] != 0)[0]) >= self.win_length):
-                    for j in range(np.amin(np.where(self.board_state[i,:] != 0)[0]),np.amax(np.where(self.board_state[i,:] != 0)[0])+self.win_length):
-                        if all(self.board_state[i,j:j+self.win_length] == 1):
-                            self.winner = 'Player 1'
-                            self.is_won = True
-                            return
-                        elif all(self.board_state[i,j:j+self.win_length] == -1):
-                            self.winner = 'Player 2'
-                            self.is_won = True
-                            return
-                else:
-                    #If a row doesn't have enough chips to win, no row above it can, either
-                    break
+
+            #Check to see if the game has been won:
+            self.check_win()
+            
+            if self.is_won == True:
+                print('The game is over; %s wins!' % self.winner)
             else:
-                break
+                #Redetermine the legal moves in the board_state:
+                self.legal_moves = np.where(self.board_state[0]==0)[0]
 
+    def check_win(self):
+        #Checks for a winning position for both players relative to self.last_move:
+        #Check for row wins
+        y,x = self.last_move
 
-        #Check for column wins
-        for j in range(0,self.num_cols):
-            #Check if column is empty
-            if any(self.board_state[:,j] != 0):
-                #Check if column has enough chips for a win
-                if (len(np.where(self.board_state[:,j] != 0)[0]) >= self.win_length):
-                    #Check for win starting at top chip (no need to look below; else game was already over.)
-                    if all(x in self.board_state[np.amin(np.where(self.board_state[:,j] != 0)):np.amin(np.where(self.board_state[:,j] != 0))+self.win_length,j] == 1):
-                        self.winner = 'Player 1'
-                        self.is_won = True
-                        return
-                    elif all(x in self.board_state[np.amin(np.where(self.board_state[:,j] != 0)):np.amin(np.where(self.board_state[:,j] != 0))+self.win_length,j] == -1):
-                        self.winner = 'Player 2'
-                        self.is_won = True
-                        return
+        #Check for column wins containing self.last_move (there can be only one):
+        if (y <= self.num_rows - self.win_length):
+            sum_temp = 0
+            for k in range(0,self.win_length):
+                sum_temp += self.board_state[y+k][x]
+            if sum_temp == self.win_length:
+                self.winner = 'Player 1'
+                self.is_won = True
+                return
+            elif sum_temp == -self.win_length:
+                self.winner = 'Player 2'
+                self.is_won = True
+                return
 
-        #Check if up-right diagonal wins are possible:
+        #Check for row wins containing self.last_move:
+        for i in range(0,self.win_length):
+            if (x-i <= self.num_cols - self.win_length and x-i >= 0):
+                sum_temp = 0
+                for k in range(0,self.win_length):
+                    sum_temp += self.board_state[y][x-i+k]
+                if sum_temp == self.win_length:
+                    self.winner = 'Player 1'
+                    self.is_won = True
+                    return
+                elif sum_temp == -self.win_length:
+                    self.winner = 'Player 2'
+                    self.is_won = True
+                    return
 
-        for i in range(np.amin(np.where(self.board_state !=0)[0]),self.num_rows - self.win_length + 1):
-            for j in range(0,num_cols - win_length + 1):
-                diag_temp_prod = 1
-                for k in range(0,win_length):
-                    diag_temp_prod = diag_temp_prod*matrix[i+k][j+k]
-                if (diag_temp_prod > diag_max_prod_down):
-                    diag_max_prod_down = diag_temp_prod
+        #Check for diagonal \\ wins containing self.last_move:
+        for i in range(0,self.win_length):
+            if (x-i <= self.num_cols-self.win_length and x-i >=0 and y-i <= self.num_rows - self.win_length and y-i >= 0):
+                sum_temp = 0
+                for k in range(0,self.win_length):
+                    sum_temp += self.board_state[y-i+k][x-i+k]
+                if sum_temp == self.win_length:
+                    self.winner = 'Player 1'
+                    self.is_won = True
+                    return
+                elif sum_temp == -self.win_length:
+                    self.winner = 'Player 2'
+                    self.is_won = True
+                    return
 
-        #Compute maximum of diagonal (down-left) products
-
-        for i in range(0,num_rows - win_length + 1):
-            for j in range(win_length - 1,num_cols):
-                diag_temp_prod = 1
-                for k in range(0,win_length):
-                    diag_temp_prod = diag_temp_prod*matrix[i+k][j-k]
-                if (diag_temp_prod > diag_max_prod_up):
-                    diag_max_prod_up = diag_temp_prod
+        #Check for diagonal // wins containing self.last_move:
+        for i in range(0,self.win_length):
+            if (x-i <= self.num_cols-self.win_length and x-i >= 0 and y+i > self.num_rows - self.win_length and y+i <= 5):
+                sum_temp = 0
+                for k in range(0,self.win_length):
+                    sum_temp += self.board_state[y+i-k][x-i+k]
+                if sum_temp == self.win_length:
+                    self.winner = 'Player 1'
+                    self.is_won = True
+                    return
+                elif sum_temp == -self.win_length:
+                    self.winner = 'Player 2'
+                    self.is_won = True
+                    return
